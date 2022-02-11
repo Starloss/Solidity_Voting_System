284985104bd72db26ec1f4f442d794935bac6999
Diego Rangel <starloss09@gmail.com>
2022-02-10 23:13:57 -0400

    Commented all entries Formated all contract

--- contracts/Voting_System.sol
+++ contracts/Voting_System.sol
@@ -69,22 +69,29 @@ contract VotingSystem is Ownable {
     /**
      *  @notice Event emitted when an Election is created by the owner
      */
-    event ElectionCreated(uint _electionID, uint _createTime, uint _endTime, string _title, string _description, address[] _candidates);
+    event ElectionCreated(
+        uint _electionID,
+        uint _createTime,
+        uint _endTime,
+        string _title,
+        string _description,
+        address[] _candidates
+    );
 
     /**
-     *  @notice Event emitted when an Election is finished.
+     *  @notice Event emitted when an Election is finished
      */
     event ElectionFinished(uint _electionID, uint[] _counters, uint[] _candidates, address _winner);
 
     /**
-     *  @notice Event emitted when an vote is casted.
+     *  @notice Event emitted when an vote is casted
      */
     event VoteCasted(uint _electionID, address _choise);
 
     /// MODIFIERS
     /**
      *  @notice Modifier function that verifies the valid status of the candidates
-     *  @notice The candidates has to be registered in the System, and can only be 5 or less.
+     *  @notice The candidates has to be registered in the System, and can only be 5 or less
      */
     modifier candidatesVerification(address[] memory _candidates) {
         require(_candidates.length > 0 && _candidates.length <= 5, "Wrong number of candidates.");
@@ -97,7 +104,7 @@ contract VotingSystem is Ownable {
     }
 
     /**
-     *  @notice Modifier function that verifies if the voter exist in the system.
+     *  @notice Modifier function that verifies if the voter exist in the system
      */
     modifier exist(address _voter) {
         require(bytes(voters[_voter].voterName).length != 0);
@@ -105,20 +112,37 @@ contract VotingSystem is Ownable {
     }
 
     /**
-     *  @notice Modifier function that verifies if the voter doesn't exist in the system.
+     *  @notice Modifier function that verifies if the voter doesn't exist in the system
      */
     modifier notExist(address _voter) {
         require(bytes(voters[_voter].voterName).length == 0);
         _;
     }
 
-    /// FUNCTIONS
-    constructor(string memory _ownerName) {
-        register(_ownerName, true);
+    /**
+     *  @notice Modifier function that verifies if the election has not ended yet
+     */
+    modifier onlyBefore(uint _electionID) {
+        require(elections[_electionID].endTime > block.timestamp, "The time for vote has finished.");
+        _;
     }
 
-    function register(string memory _voterName, bool _valid) public notExist(msg.sender) {
-        require(bytes(_voterName).length != 0 && _valid);
+    /// FUNCTIONS
+    /**
+     *  @notice Constructor function that initialice the contract and register the owner as first voter
+     *  @param _ownerName must be an string with length > 0, wich will be used to register the owner as a voter
+     */
+    constructor(string memory _ownerName) {
+        register(_ownerName);
+    }
+
+    /**
+     *  @notice Function that allows to register a voter with a name
+     *  @dev Every address could only call this function once
+     *  @param _voterName must be an string with length > 0
+     */
+    function register(string memory _voterName) public notExist(msg.sender) {
+        require(bytes(_voterName).length != 0);
     
         voter memory v;
         v.voterName = _voterName;
@@ -129,14 +153,27 @@ contract VotingSystem is Ownable {
         emit VoterRegistered(totalVoters);
     }
 
-    function createNewVote(address[] memory _candidates)
+    /**
+     *  @notice Function that allows to the Owner create an election
+     *  @param _candidates must be an array with 0 < length <= 5 of addresses registered in the System
+     */
+    function createNewElection(address[] memory _candidates)
         public
         onlyOwner
         candidatesVerification(_candidates)
     {
-
     }
 
-    function doVote(uint _election, address _choise) public exist(msg.sender) {
+    /**
+     *  @notice Function that allows the user vote in an election
+     *  @dev Every address could only call this function once for every election
+     *  @param _electionID must be an integer representing the ID of the election created by the owner
+     *  @param _choise is the address of the account who the user wants to win the election
+     */
+    function doVote(uint _electionID, address _choise)
+        public
+        exist(msg.sender)
+        onlyBefore(_electionID)
+    {
     }
 }
